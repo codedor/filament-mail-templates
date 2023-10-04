f2a37cb9e072c27fcb66541fa115a06421302b8e
Louise Devolder <louise.devolder@onlyhumans.com>
2023-10-04 07:29:44 +0000

    add styling locale select & add basic template without styling

--- resources/views/filament/pages/preview.blade.php
+++ resources/views/filament/pages/preview.blade.php
@@ -1,12 +1,33 @@
 <x-filament::page>
     {{ $record->description }}
 
-    <div class="w-full">
-        <select wire:model.live="currentLocale">
-            @foreach ($locales as $locale)
-                <option value="{{ $locale }}">{{ $locale }}</option>
-            @endforeach
-        </select>
+    <div class="w-full flex">
+        <div class="
+            fi-input-wrp
+            rounded-lg shadow-sm bg-white
+            ring-1 ring-gray-950/10
+            transition duration-75
+            focus-within:ring-2 focus-within:ring-primary-600
+            dark:bg-white/5
+            dark:ring-white/20 dark:focus-within:ring-primary-500 fi-fo-select"
+        >
+            <select
+                class="
+                    fi-select-input
+                    block py-1.5 pe-8 ps-3
+                    text-base text-gray-950
+                    border-none bg-transparent
+                    transition duration-75
+                    focus:ring-0
+                    disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] dark:text-white dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)]
+                    sm:text-sm sm:leading-6 [&_optgroup]:bg-white [&_optgroup]:dark:bg-gray-900 [&_option]:bg-white [&_option]:dark:bg-gray-900"
+                wire:model.live="currentLocale"
+            >
+                @foreach ($locales as $locale)
+                    <option value="{{ $locale }}">{{ $locale }}</option>
+                @endforeach
+            </select>
+        </div>
     </div>
 
     <div class="mt-4 w-full border border-lg bg-white" x-data="{
--- resources/views/mail/template.blade.php
+++ resources/views/mail/template.blade.php
@@ -5,9 +5,128 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="color-scheme" content="light">
         <meta name="supported-color-schemes" content="light">
+
+        <style>
+            @media only screen and (max-width: 600px) {
+                .inner-body {
+                    width: 100% !important;
+                }
+
+                .footer {
+                    width: 100% !important;
+                }
+            }
+
+            /* Base */
+            body {
+                margin: 0;
+                width: 100%;
+                height: 100%;
+
+                hyphens: auto;
+                -moz-hyphens: auto;
+                -ms-word-break: break-all;
+                -webkit-hyphens: auto;
+                -webkit-text-size-adjust: none;
+                word-break: break-all;
+                word-break: break-word;
+            }
+
+            .wrapper,
+            .content {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+
+                -premailer-cellpadding: 0;
+                -premailer-cellspacing: 0;
+                -premailer-width: 100%;
+            }
+
+            /* Body */
+            .inner-body {
+                margin: 0 auto;
+                padding: 0;
+                width: 570px;
+
+                -premailer-cellpadding: 0;
+                -premailer-cellspacing: 0;
+                -premailer-width: 570px;
+            }
+        </style>
     </head>
 
     <body>
-        {{ $body }}
+        <table
+            class="wrapper"
+            width="100%"
+            cellpadding="0"
+            cellspacing="0"
+            role="presentation"
+        >
+            <tr>
+                <td align="center">
+                    <table
+                        class="content"
+                        width="100%"
+                        cellpadding="0"
+                        cellspacing="0"
+                        role="presentation"
+                    >
+                        <tr>
+                            <td>
+                                <div class="inner-body">
+                                    <!-- Header -->
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td
+                                class="content"
+                                width="100%"
+                                cellpadding="0"
+                                cellspacing="0"
+                            >
+                                <table
+                                    class="inner-body"
+                                    align="center"
+                                    width="640"
+                                    cellpadding="0"
+                                    cellspacing="0"
+                                    role="presentation"
+                                >
+                                    <!-- Body content -->
+                                    <tr>
+                                        <td class="content-cell">
+                                            {{ $body }}
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table
+                                    class="footer"
+                                    align="center"
+                                    width="570"
+                                    cellpadding="0"
+                                    cellspacing="0"
+                                    role="presentation"
+                                >
+                                    <tr>
+                                        <td align="center">
+                                            <div class="inner-body">
+                                                <!-- Footer -->
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
     </body>
 </html>
